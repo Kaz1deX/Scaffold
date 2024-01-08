de4db0ce536edaada2fb628ed9e04f277eb2ced2
Kaz1deX <max2003255@gmail.com>
2024-01-08 22:21:30 +0300

    SnackBar

--- app/src/main/java/com/example/topappbar/MainActivity.kt
+++ app/src/main/java/com/example/topappbar/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -17,17 +19,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDefaults
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.topappbar.ui.theme.MyGreen
 import com.example.topappbar.ui.theme.TopAppBarTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +55,24 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(context: Context) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(snackbarHostState) {data ->
+                Snackbar(
+                    containerColor = MyGreen,
+                    snackbarData = data,
+                    shape = RoundedCornerShape(20.dp),
+                    contentColor = Color.Black,
+                    modifier = Modifier
+                        .padding(bottom = 50.dp)
+                )
+            }
+        },
         topBar = {
             TopAppBar(
                 title = {
@@ -65,7 +93,15 @@ fun MainScreen(context: Context) {
                 actions = {
                     IconButton(
                         onClick = {
-                            Toast.makeText(context, "Delete", Toast.LENGTH_SHORT).show()
+                            coroutineScope.launch {
+                                val result = snackbarHostState.showSnackbar(
+                                    message = "Item deleted!",
+                                    actionLabel = "Undone"
+                                )
+                                if (result == SnackbarResult.ActionPerformed) {
+                                    Toast.makeText(context, "Item recovered", Toast.LENGTH_SHORT).show()
+                                }
+                            }
                         }
                     ) {
                         Icon(
